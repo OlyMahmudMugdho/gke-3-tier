c3c7213d8e4108d7999c8e29b7fc1b5617b7dea0
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-06-17 16:54:23 +0000

    ingress class updated, bug fix

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,8 +1,9 @@
-kubectl apply -f namespace.yaml
-kubectl apply -f secret.yaml
-kubectl apply -f db-pvc.yaml
-kubectl apply -f db-service.yaml
-kubectl apply -f db-statefulset.yaml
-kubectl apply -f backend-service.yaml
-kubectl apply -f backend-deployment.yaml
-kubectl apply -f ingress.yaml
+kubectl apply -f ../k8s/namespace.yaml
+kubectl apply -f ../k8s/secret.yaml
+kubectl apply -f ../k8s/db-pvc.yaml
+kubectl apply -f ../k8s/db-service.yaml
+kubectl apply -f ../k8s/db-statefulset.yaml
+kubectl apply -f ../k8s/backend-service.yaml
+kubectl apply -f ../k8s/backend-deployment.yaml
+kubectl apply -f ../k8s/ingress.yaml
+
