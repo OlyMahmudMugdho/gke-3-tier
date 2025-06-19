e9c649e29855a28aa42209244a0713a85c1fa78f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-06-19 12:34:47 +0000

    add argocd installation script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl create ns argocd
+./install_argocd.sh && \
 ./install_nginx_ingress_controller.sh && \
 ./install_kube_prometheus_stack.sh && \
 kubectl apply -f ../argocd/argocd-app.yaml
