a6d37386942064150220d6718536e854657afef5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-06-19 07:58:19 +0000

    expose grafana and prometheus

--- scripts/install_kube_prometheus_stack.sh
+++ scripts/install_kube_prometheus_stack.sh
@@ -3,6 +3,7 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update && \
 helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitor && \
 kubectl expose deployment kube-prometheus-stack-grafana --port=3000 --target-port=3000 --name=grafana --type=LoadBalancer -n monitor && \
+kubectl expose service kube-prometheus-stack-prometheus -n monitor --name=prometheus-lb --port=9090 --target-port=9090 --type=LoadBalancer && \
 echo "kube-prometheus stack installed and applied successfully" && \
 echo "Username: admin" && \
 echo "Password: prom-operator" && \
